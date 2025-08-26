d588f52829bcfee3ab973b656459f313b25d769f
henry <henry.pham@sotatek.com>
2025-08-26 08:40:54 +0700

    fix: hydration error on light/dark switcher

--- apps/docs/components/ThemeSwitcher.tsx
+++ apps/docs/components/ThemeSwitcher.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useMounted } from "@/hooks/useMounted";
 import { Button } from "@workspace/ui/components/button";
 import { MoonStar, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
@@ -11,6 +12,7 @@ enum Theme {
 
 export function ThemeSwitcher() {
   const { theme, setTheme } = useTheme();
+  const mounted = useMounted();
 
   return (
     <Button
@@ -18,7 +20,7 @@ export function ThemeSwitcher() {
       size="icon"
       onClick={() => setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)}
     >
-      {theme === Theme.DARK ? <SunIcon /> : <MoonStar />}
+      {mounted && (theme === Theme.DARK ? <SunIcon /> : <MoonStar />)}
     </Button>
   );
 }
